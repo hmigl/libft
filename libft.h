07eb8c9c5d8f8993677c49d160d3df01f28176b3
ahugom <88746406+ahugom@users.noreply.github.com>
2021-08-22 20:58:14 -0300

    Header file update

--- libft.h
+++ libft.h
@@ -27,6 +27,10 @@ void	*ft_memset(void *s, int c, size_t n);
 
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+
 size_t	ft_strlen(const char *s);
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
