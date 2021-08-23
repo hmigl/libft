f26de3c6c50b479f5588741d355e3ed6ef6a3505
ahugom <88746406+ahugom@users.noreply.github.com>
2021-08-23 20:45:23 -0300

    New functions

---
+++ ft_memchr.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	char	*str;
+
+	str = (char *) s;
+	while (n--)
+		if (*str++ == c)
+			return (--str);
+	return (0);
+}
---
+++ ft_strchr.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	if ((char) c == '\0')
+		return ((char *) s + ft_strlen(s));
+	while (*s)
+		if (*s++ == (char) c)
+			return ((char *)--s);
+	return (0);
+}
