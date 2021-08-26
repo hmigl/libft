9585c4ca8d5a9b521869a36326005825402e494d
ahugom <88746406+ahugom@users.noreply.github.com>
2021-08-26 18:09:39 -0300

    Update functions list

--- libft.h
+++ libft.h
@@ -2,6 +2,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <limits.h>
 
 /* <ctype.h> */
 int		ft_isalpha(int c);
@@ -15,11 +16,13 @@ int		ft_toupper(int c);
 /* <string.h> */
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strdup(const char *s);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
@@ -28,5 +31,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 /* <stdlib.h> */
 int		ft_atoi(const char *nptr);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
