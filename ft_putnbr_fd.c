42b1acff4be2716c86e5e1f1a30474ec5e92a8d9
ahugom <88746406+ahugom@users.noreply.github.com>
2021-08-27 16:53:46 -0300

    First part 2 functions

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == INT_MIN)
+		ft_putstr_fd("-2147483648", fd);
+	else if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(n * -1, fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
+}
