8cd3df80405407d0b8abe75a1b645d2f1c3459cd
ahugom <88746406+ahugom@users.noreply.github.com>
2021-08-30 20:14:56 -0300

    One more function of part 2

---
+++ ft_itoa.c
@@ -0,0 +1,40 @@
+#include "libft.h"
+
+static int	ft_numlen(int num)
+{
+	if (!(num / 10))
+		return (1);
+	return (1 + ft_numlen(num / 10));
+}
+
+/*
+ * Allocates (MALLOC()) and retuns a string
+ * representing the integer received. Handles negative
+ * nums
+ */
+char	*ft_itoa(int n)
+{
+	char	*str;
+	size_t	nlen;
+	int		sig;
+
+	if (n == INT_MIN)
+		return (ft_strdup("-2147483648"));
+	nlen = ft_numlen(n);
+	sig = n < 0;
+	str = (char *)malloc((sig + nlen + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	if (sig)
+	{
+		n *= -1;
+		str[0] = '-';
+	}
+	str[sig + nlen] = '\0';
+	while (nlen--)
+	{
+		str[sig + nlen] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
+}
