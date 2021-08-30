1ad0d4f1d67c2d5676668cbf8668769329ce3f20
ahugom <88746406+ahugom@users.noreply.github.com>
2021-08-30 20:03:39 -0300

    New prototype

--- libft.h
+++ libft.h
@@ -42,5 +42,6 @@ void	ft_putnbr_fd(int n, int fd);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 
 #endif
