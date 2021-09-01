a8f02239dca7df54d895e16fa71370cdb60a7cba
ahugom <88746406+ahugom@users.noreply.github.com>
2021-09-01 20:40:57 -0300

    More part 2 functions

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ FILES = ft_isalpha.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_tolower.c \
 	ft_strchr.c ft_strrchr.c ft_strnstr.c ft_strlcpy.c ft_strlcat.c \
 	ft_memmove.c ft_calloc.c ft_strdup.c ft_putchar_fd.c ft_putstr_fd.c \
 	ft_putendl_fd.c ft_putnbr_fd.c ft_substr.c ft_strjoin.c ft_strtrim.c \
-	ft_itoa.c
+	ft_itoa.c ft_strmapi.c ft_striteri.c
 
 OBJ = $(FILES:c=o)
 RM = rm -f
---
+++ ft_strmapi.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+/*
+ * Applies the function 'f' to each char of the string 's'
+ * to create a new string (whith MALLOC()) resulting
+ * from successive applications of 'f'
+ */
+char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
+{
+	unsigned int	i;
+	size_t			slen;
+	char			*str;
+
+	if (s && f)
+	{
+		i = -1;
+		slen = ft_strlen(s);
+		str = (char *)malloc(slen + 1 * sizeof(char));
+		if (!str)
+			return (NULL);
+		while (++i < slen)
+			str[i] = f(i, s[i]);
+		str[i] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
