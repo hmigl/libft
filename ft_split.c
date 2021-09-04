278418d957067d6f478b600742efc860ae4e6a29
ahugom <88746406+ahugom@users.noreply.github.com>
2021-09-04 11:46:12 -0300

    Last mandatory part function

---
+++ ft_split.c
@@ -0,0 +1,88 @@
+#include "libft.h"
+
+static size_t	ft_wdscnt(char const *s, char c)
+{
+	size_t	wc;
+	size_t	iswd;
+
+	wc = 0;
+	iswd = 1;
+	while (*s)
+	{
+		if (iswd && *s != c)
+		{
+			iswd = 0;
+			wc++;
+		}
+		else if (*s == c)
+			iswd = 1;
+		++s;
+	}
+	return (wc);
+}
+
+static size_t	ft_elsize(const char *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+static char	*ft_wdfactory(char const *s, char c)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = ft_elsize(s, c);
+	str = (char *)malloc((i + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	j = 0;
+	while (i-- && s[j] != c)
+	{
+		str[j] = s[j];
+		j++;
+	}
+	str[j] = 0;
+	return (str);
+}
+
+static void	*ft_free_it(char **it)
+{
+	if (!it)
+		return (NULL);
+	while (*it != NULL)
+		free(*it++);
+	free(it);
+	return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**array;
+	size_t	wds;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	wds = ft_wdscnt(s, c);
+	array = (char **)malloc((wds + 1) * sizeof(char *));
+	if (!array)
+		return (NULL);
+	i = -1;
+	while (++i < wds)
+	{
+		while (*s && *s == c)
+			s++;
+		array[i] = ft_wdfactory(s, c);
+		if (!array)
+			return (ft_free_it(array));
+		s += ft_elsize(s, c) + 1;
+	}
+	array[i] = NULL;
+	return (array);
+}
